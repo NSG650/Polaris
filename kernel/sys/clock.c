83a07e38d3fd591ab47ef2b7381e6b8fd617aa51
NSG650 <nintendoswitchguy650@gmail.com>
2021-08-02 13:00:32 +0530

    le time at last

--- kernel/sys/clock.c
+++ kernel/sys/clock.c
@@ -15,34 +15,65 @@ static unsigned char read(int reg) {
     return port_byte_in(0x71);
 }
 
-unsigned char clock_get_seconds(void) {
-    unsigned char seconds = read(0);
-    unsigned char second = (seconds & 0x0F) + ((seconds / 16) * 10);
-    return second;
+static int bcdtobin(int val) {
+    return (val & 0xF) + (val >> 4) * 10;
 }
 
-unsigned char clock_get_minutes(void) {
-    unsigned char minutes = read(0x2);
-    unsigned char minute = (minutes & 0x0F) + ((minutes / 16) * 10);
-    return minute;
+static time rtc_get_time() {
+    time ret_time;
+    ret_time.hour = read(0x4);
+    ret_time.minute = read(0x2);
+    ret_time.second = read(0);
+    printf("The time is %d:%d:%d\n", bcdtobin(ret_time.hour), bcdtobin(ret_time.minute), bcdtobin(ret_time.second));
+    return ret_time;
 }
 
-unsigned char clock_get_hours(void) {
-    unsigned char hours = read(0x4);
-    unsigned char hour = ((hours & 0x0F) + (((hours & 0x70) / 16) * 10)) | (hours & 0x80);
-    return hour;
+static datetime_t rtc_get_date_time() {
+    datetime_t date_time;
+
+    date_time.day = read(0x7);
+    date_time.month = read(0x8);
+    date_time.year = read(0x9);
+
+    date_time.time = rtc_get_time();
+
+    int registerB = read(0x0B);
+
+    // BCD conversion
+
+    if (~registerB & registerB_DataMode) {
+        date_time.time.second = bcdtobin(date_time.time.second);
+        date_time.time.minute = bcdtobin(date_time.time.minute);
+        date_time.time.hour = bcdtobin(date_time.time.hour);
+        date_time.day = bcdtobin(date_time.day);
+        date_time.month = bcdtobin(date_time.month);
+        date_time.year = bcdtobin(date_time.year);
+    }
+
+    // Convert 12 hour clock to 24 hour clock if necessary
+
+    if (!(registerB & 0x02) && (date_time.time.hour & 0x80)) {
+        date_time.time.hour = ((date_time.time.hour & 0x7F) + 12) % 24;
+    }
+
+    // Calculate the full (4-digit) year
+
+    date_time.year += 2000;
+    printf("The date is %d\\%d\\%d\n", date_time.day, date_time.month, date_time.century);
+    return date_time;
 }
 
 uint64_t get_unix_timestamp(void) {
-   uint32_t y, m, d;
-   uint64_t t;
-   y = read(0x9) + 2000; // do you really think this project will exist in 2100?
-   m = read(0x8);
-   d = read(0x7);
-   if(m <= 2) {
-       m += 12;
-       y -= 1;
-   }
+    datetime_t datetime = rtc_get_date_time();
+    uint32_t y, m, d;
+    y = datetime.year;
+    m = datetime.month;
+    d = datetime.day;
+    uint64_t t;
+    if(m <= 2) {
+        m += 12;
+        y -= 1;
+    }
     //Convert years to days
     t = (365 * y) + (y / 4) - (y / 100) + (y / 400);
     //Convert months to days
@@ -52,7 +83,7 @@ uint64_t get_unix_timestamp(void) {
     //Convert days to seconds
     t *= 86400;
     //Add hours, minutes and seconds
-    t += (3600 * clock_get_hours()) + (60 * clock_get_minutes()) + clock_get_seconds();
+    t += (3600 * datetime.time.hour) + (60 * datetime.time.minute) + datetime.time.second;
 
     return t;
 }
--- kernel/sys/clock.h
+++ kernel/sys/clock.h
@@ -1,7 +1,23 @@
-#ifndef CLOCK_H
-#define CLOCK_H
+#ifndef RTC_H
+#define RTC_H
 
 #include <stdint.h>
+
+#define registerB_DataMode (1 << 2)
+
+typedef struct {
+    uint32_t hour;
+    uint32_t second;
+    uint32_t minute;
+} time;
+
+typedef struct {
+    uint32_t day;
+    uint32_t month;
+    uint32_t year;
+    uint32_t century;
+    time time;
+} datetime_t;
 
 uint64_t get_unix_timestamp(void);
 
