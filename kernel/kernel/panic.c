9a6162b3d24794c4123b5166f31eb1f18076628e
NSG650 <nintendoswitchguy650@gmail.com>
2021-08-05 13:48:09 +0530

    panic with RIP and RBP

--- kernel/kernel/panic.c
+++ kernel/kernel/panic.c
@@ -7,14 +7,17 @@
 void panic(char message[], char file[], char assert, uint32_t line) {
 	char x[1024];
 	vid_reset();
+	uint8_t* rip = __builtin_return_address(0);
+    uint64_t* rbp = __builtin_frame_address(0);
 	if (assert) {
 		clear_screen(0x00B800);
-		sprintf(x, "*** ASSERTION FAILURE: %s\nFile: %s\nLine: %u", message,
-				file, line);
+		sprintf(x, "*** ASSERTION FAILURE: %s\nFile: %s\nLine: %u\nRIP: 0x%p\nRBP: 0x%p\n", message,
+				file, line, rip, rbp);
 		kprintbgc(x, 0xFFFFFF, 0x00B800);
 	} else {
 		clear_screen(0xB80000);
-		sprintf(x, "*** PANIC: %s\nFile: %s\nLine: %u", message, file, line);
+		sprintf(x, "*** PANIC: %s\nFile: %s\nLine: %u\nRIP: 0x%p\nRBP: 0x%p\n", message,
+				file, line, rip, rbp);
 		kprintbgc(x, 0xFFFFFF, 0xB80000);
 	}
 	write_serial(x);
--- kernel/kernel/panic.h
+++ kernel/kernel/panic.h
@@ -1,6 +1,8 @@
 #ifndef PANIC_H
 #define PANIC_H
 
+#include <stdint.h>
+
 void panic(char message[], char file[], char assert, uint32_t line);
 
 #define PANIC(b)  (panic(b, __FILE__, 0, __LINE__));
