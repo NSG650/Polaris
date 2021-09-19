c1130acf1c8397f6ac128226cc59eec3a75087ec
Sebastian-byte <sebastianmontoya209@gmail.com>
2021-09-18 19:26:30 -0500

    Cleanup serial driver

--- kernel/serial/serial.h
+++ kernel/serial/serial.h
@@ -18,16 +18,10 @@
  * limitations under the License.
  */
 
-#include <stdint.h>
-
 #define COM1 0x3F8
 
-void write_serial_port_char(uint16_t PORT, char word);
-void write_serial_port(uint16_t PORT, char *word);
-int serial_install_port(uint16_t PORT);
-void write_serial_char(char word);
-char read_serial(uint16_t PORT);
-void write_serial(char *word);
 void serial_install(void);
+void write_serial(char *word);
+void write_serial_char(char word);
 
 #endif
