7cef83e3b7f0a04bf2db24a77919db7356feb2b2
NSG650 <51860844+NSG650@users.noreply.github.com>
2021-12-10 10:54:01 +0530

    swapped out fib for fast_fib in test.c

--- root/test.c
+++ root/test.c
@@ -7,6 +7,31 @@ char other_array[16] = {0};
 
 int some_var = 123;
 
+void fast_fib(unsigned long long int n, unsigned long long int m[]) {
+    unsigned long long int a, b, c, d;
+    if(n == 0) {
+        m[0] = 0;
+        m[1] = 1;
+        return;
+    }
+    unsigned long long int s[2] = {0};
+    fast_fib(n / 2, s);
+    a = s[0]; // F(N)
+    b = s[1]; // F(N + 1)
+    c = a * (b * 2 - a);
+    d = a * a + b * b;
+    if(!(n & 1)) {
+        m[0] = c;
+        m[1] = d;
+        return;
+    }
+    else {
+        m[0] = d;
+        m[1] = c + d;
+        return;
+    }
+}
+
 static long long fib(long long i) {
 	if (i <= 1)
 		return i;
@@ -26,6 +51,8 @@ void run() {
 	other_func();
 	printf("other_array[0]: %d\n", other_array[0]);
 	printf("some_var: %X\n", some_var);
-	printf("fib(10): %lld\n", fib(10));
+	unsigned long long int s[2] = {0};
+	fast_fib(10, s);
+	printf("fast_fib(10, s): n: %llu, n + 1: %llu", s[0], s[1]);
 	thread_exit(0);
 }
