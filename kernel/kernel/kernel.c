5fc3609ddb4cc293fff0b734d0328e02baa8a6bd
NSG650 <nsg650@waifu.club>
2022-05-30 17:37:15 +0530

    kernel: shift usermode thread to c code now that stack access issues are fixed

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -3,7 +3,17 @@
 #include <sched/sched.h>
 #include <sys/prcb.h>
 
-extern void user_thread(void);
+void user_thread(void) {
+	asm volatile(
+		"xor rax, rax\n"
+		"mov rdi, 0x41\n"
+		"int 0x80\n"
+		"mov rdi, 0xa\n"
+		"int 0x80\n"
+	);
+	for (;;)
+		;
+}
 
 void kernel_main(void *args) {
 	// thread_create((uintptr_t)user_thread, 0, 1, prcb_return_current_cpu()->running_thread->mother_proc);
