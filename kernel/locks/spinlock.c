771d64373a3e35e332fc86e41e9029d757932a0b
Sebastian <sebastianmontoya209@gmail.com>
2022-06-30 17:25:07 -0500

    kernel: make spinlock for loop infinite

--- kernel/locks/spinlock.c
+++ kernel/locks/spinlock.c
@@ -19,11 +19,7 @@
 #include <locks/spinlock.h>
 
 bool spinlock_acquire(lock_t spin) {
-	if (__atomic_test_and_set(&spin, __ATOMIC_ACQUIRE)) {
-		return false;
-	} else {
-		return true;
-	}
+	return !__atomic_test_and_set(&spin, __ATOMIC_ACQUIRE);
 }
 
 void spinlock_acquire_or_wait(lock_t spin) {
@@ -32,8 +28,8 @@ retry_lock:
 		return;
 	}
 
-	// Do a rough wait until the lock is free for cache-locality.
-	for (int i = 0; i < 50000000; i++) {
+	// Do a rough wait until the lock is free for cache-locality
+	for (;;) {
 		if (__atomic_load_n(&spin, __ATOMIC_RELAXED) == 0) {
 			goto retry_lock;
 		}
