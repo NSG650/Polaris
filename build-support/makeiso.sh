7dbd5402b81da78d485cd91db2fb046f0f6e8b84
Neptune <neptune650@proton.me>
2025-06-20 14:36:33 -0500

    boot: update Limine to 9.3.3

--- build-support/makeiso.sh
+++ build-support/makeiso.sh
@@ -3,7 +3,7 @@
 set -ex
 
 rm -rf sysroot
-./jinx sysroot
+./jinx install sysroot kernel drivers init
 ./jinx host-build limine
 
 ( cd sysroot && tar -cf ../ramdisk.tar * )
@@ -11,7 +11,7 @@ rm -rf sysroot
 rm -rf iso_root
 mkdir -v iso_root
 cp sysroot/usr/share/polaris/polaris.elf iso_root/polaris.elf
-cp build-support/limine.cfg iso_root/
+cp build-support/limine.conf iso_root/
 cp ramdisk.tar iso_root/
 
 cp host-pkgs/limine/usr/local/share/limine/limine-bios.sys iso_root/
@@ -20,8 +20,10 @@ cp host-pkgs/limine/usr/local/share/limine/limine-uefi-cd.bin iso_root/
 mkdir -pv iso_root/EFI/BOOT
 cp host-pkgs/limine/usr/local/share/limine/BOOT*.EFI iso_root/EFI/BOOT/
 
-xorriso -as mkisofs -b limine-bios-cd.bin -no-emul-boot -boot-load-size 4 \
-	-boot-info-table --efi-boot limine-uefi-cd.bin -efi-boot-part \
-	--efi-boot-image --protective-msdos-label iso_root -o polaris.iso
+xorriso -as mkisofs -R -r -J -b limine-bios-cd.bin \
+        -no-emul-boot -boot-load-size 4 -boot-info-table -hfsplus \
+        -apm-block-size 2048 --efi-boot limine-uefi-cd.bin \
+        -efi-boot-part --efi-boot-image --protective-msdos-label \
+        iso_root -o polaris.iso
 
 host-pkgs/limine/usr/local/bin/limine bios-install polaris.iso
