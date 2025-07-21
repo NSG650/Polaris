261af008912f6228f10eae7f989a66e1da179160
NSG650 <nsg650@outlook.com>
2025-07-21 16:35:15 +0530

    meta: add openrc port

--- tests/kaslr_bypass.c
+++ tests/kaslr_bypass.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #define STEP (0x1000 * 0x100)
-#define KERNEL_BASE_START  (0xffffffff80000000)
+#define KERNEL_BASE_START (0xffffffff80000000)
 #define KERNEL_BASE_END (0xffffffffffffffff)
 #define RANGE_SIZE (KERNEL_BASE_END - KERNEL_BASE_START) / STEP
 #define ITER 250
@@ -12,30 +12,31 @@
 extern uint64_t side_channel(uintptr_t address);
 
 int main(void) {
-    uint64_t times[RANGE_SIZE] = {0};
-    uintptr_t addresses[RANGE_SIZE] = {0};
+	uint64_t times[RANGE_SIZE] = {0};
+	uintptr_t addresses[RANGE_SIZE] = {0};
 
-    for (size_t i = 0; i < ITER + 5; i++) {
-        for (size_t idx = 0; idx < RANGE_SIZE; idx++) {
-            getpid(); // Doing a system call improves the chances of getting the kernel base
-            addresses[idx] = KERNEL_BASE_START + idx * STEP;
-            // This is a warm up before we actually start recording things
-            if (i <= 5) {
-                times[idx] += side_channel(addresses[idx]);
-            }
-        }
-    }
+	for (size_t i = 0; i < ITER + 5; i++) {
+		for (size_t idx = 0; idx < RANGE_SIZE; idx++) {
+			getpid(); // Doing a system call improves the chances of getting the
+					  // kernel base
+			addresses[idx] = KERNEL_BASE_START + idx * STEP;
+			// This is a warm up before we actually start recording things
+			if (i <= 5) {
+				times[idx] += side_channel(addresses[idx]);
+			}
+		}
+	}
 
-    uint64_t minimum_time = (uint64_t)-1;
-    uintptr_t most_likely_kernel_base = 0;
+	uint64_t minimum_time = (uint64_t)-1;
+	uintptr_t most_likely_kernel_base = 0;
 
-    for (size_t i = 0; i < RANGE_SIZE; i++) {
-        if (minimum_time > times[i] / ITER) {
-            minimum_time = times[i] / ITER;
-            most_likely_kernel_base = addresses[i];
-        }
-    }
+	for (size_t i = 0; i < RANGE_SIZE; i++) {
+		if (minimum_time > times[i] / ITER) {
+			minimum_time = times[i] / ITER;
+			most_likely_kernel_base = addresses[i];
+		}
+	}
 
-    printf("Kernel base most likely to be: %p\n", most_likely_kernel_base);
-    return 0;
+	printf("Kernel base most likely to be: %p\n", most_likely_kernel_base);
+	return 0;
 }
